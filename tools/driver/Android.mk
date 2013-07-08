bcc210f66b4283eb09d7054388cc3e8e4b614829
David Eddlemon <neo@TheMatrix.com>
2013-07-09 03:18:25 +0400

    Strict Aliasing Fix. Optional Compiler Flags Fix

--- tools/driver/Android.mk
+++ tools/driver/Android.mk
@@ -85,6 +85,8 @@ LOCAL_STATIC_LIBRARIES := \
 
 LOCAL_LDLIBS += -lpthread -lm -ldl
 
+LOCAL_CFLAGS := -fno-strict-aliasing $(call-cc-option,-Qunused-arguments)
+
 include $(CLANG_HOST_BUILD_MK)
 include $(CLANG_TBLGEN_RULES_MK)
 include $(BUILD_HOST_EXECUTABLE)
